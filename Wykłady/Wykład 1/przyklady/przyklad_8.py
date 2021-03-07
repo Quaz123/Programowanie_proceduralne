d96a69af993be6f38e51f0ee2888607ae06b3df6
Quaz123 <csi88978@cndps.com>
2021-03-07 16:50:25 +0100

    a

--- Wykłady/Wykład 1/przyklady/przyklad_8.py
+++
@@ -1,7 +0,0 @@
-# kacze typowanie
-
-print(len("Ala ma kota"))           # argument jest ciągiem tekstowym
-print(len((1,2,3,4,5)))             # argument jest krotką
-print(len(["a","b", "c"]))          # argument jest listą
-print(len({1,2,3,4}))               # argument jest zbiorem
-print(len({"one":1, "two":2}))      # argument jest słownikiem
